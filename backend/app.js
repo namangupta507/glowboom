b0753552139d9cf0e6593c4c43423b09520ae2e7
Naman Gupta <naman.gupta507@gmail.com>
2024-07-10 15:01:29 +0530

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors=require('cors');
 const connectDB=require('./db/connection');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,8 +1,9 @@
 const mongoose=require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://namangupta507:HukNN0DvZaVJ19Vu@cluster0.wwgobke.mongodb.net/adminGlow?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
