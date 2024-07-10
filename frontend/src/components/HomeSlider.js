35bc1c2efb824b2013733e2496a06b67672ae167
Naman Gupta <naman.gupta507@gmail.com>
2024-07-10 11:51:20 +0530

    final changes

--- frontend/src/components/HomeSlider.js
+++ frontend/src/components/HomeSlider.js
@@ -2,10 +2,10 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import slide1 from '../assets/a5.png';
-import slide2 from '../assets/a6.png';
-import slide3 from '../assets/a8.png';
-import slide4 from '../assets/a10.png';
+import slide1 from '../assets/ss4.jpg';
+import slide2 from '../assets/ss1.jpg';
+import slide3 from '../assets/ss2.jpg';
+import slide4 from '../assets/ss3.jpg';
 import '../components/HomeSlider.css';
 
 export const HomeSlider=()=> {
