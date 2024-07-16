4846cc5090876a401dd7af091b81a0d1f5aaa930
Naman Gupta <naman.gupta507@gmail.com>
2024-07-16 11:51:26 +0530

    dfgifurf

--- frontend/src/components/HairCare.js
+++ frontend/src/components/HairCare.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from 'react';
+import "../components/HairCare.css";
+import image from "../assets/hh.jpeg";
+import { HairCareProducts } from './HairCareProducts';
 
 export const HairCare = () => {
   return (
-    <div>HairCare</div>
+    <div className='haircare'>
+        <div className='haircare-top'>
+        <h2>Skin Care</h2>
+      </div>
+      <div className='skincare-section-2'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6'>
+                <div className='haircare-section-2-left'>
+                  <div className='haircare-section-2-left-header'>
+                        <span className='left-separator'></span>
+                        
+                        <div className='haircare-section-2-left-header-heading'>
+                          <h2>Hair Care
+                          </h2>
+                        </div>
+
+                         <span className='right-separator'></span>
+                    </div>
+                    <div className='haircare-section-2-left-header-heading2'>
+                      <p>Committed to Quality</p>
+                    </div>
+                  <div className='haircare-section-2-left-content'>
+                    <p>Hair products encompass a diverse range of items designed to care for and style hair. This category includes shampoos, conditioners, lotions, hair serums, hair masks, hair oils, hair pack, hair colour and more. These products serve various purposes such as cleansing, moisturizing, repairing, volumizing, and providing hold or texture to achieve desired hairstyles.</p>
+                  </div>
+                </div>
+            </div>
+            <div className='col-md-6'>
+              <div className='haircare-section-2-left'>
+                <div className='image-container'>
+                  <img src={image} alt="haircare"></img>
+                </div>
+              </div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className='haircare-section3'>
+        <div className='container'>
+          <div className='haircare-section3-heading'>
+                        <span className='left-separator'></span>
+                        
+                        <div className='haircare-section-2-left-header-heading'>
+                          <h2>Our products are
+                          </h2>
+                        </div>
+
+                         <span className='right-separator'></span>
+        </div>
+        <div className='haircare-section3-products'>
+          <HairCareProducts/>
+        </div>
+          </div>
+        </div>
+    </div>
   )
 }
---
+++ frontend/src/components/HairCareProducts.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export const HairCareProducts = () => {
+    const [skinCareProducts, setSkinCareProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await axios.get('https://glowboom.onrender.com/allproducts');
+                const filtered = res.data.filter(product => product.category.includes('Hair Care'));
+                setSkinCareProducts(filtered);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+   
+    return (
+      
+           
+            <div className="products-main">
+                <div className="row">
+                    {skinCareProducts.map((product) => (
+                        <div className="col-md-3 mb-5" key={product._id}>
+                            <div className="products-block">
+                                {product?.images?.map((image, index) => (
+                                    <div className="image-block" key={index}>
+                                        <img className='product_image' src={`https://glowboom.onrender.com/${image}`} alt="product_image" />
+                                    </div>
+                                ))}
+                                <div className="inner-block">
+                                    <p className="product_category">{product.category}</p>
+                                    <p className="product_title">{product.title}</p>
+                                    <p className="product_price">₹ {product.price}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        
+    );
+};
